8835f5a68213bd4289dbc6ba4116d5b54673714d
Noah Segal <n.h.segal@gmail.com>
2022-09-03 15:47:44 +0900

    Add radio buttons to cards

--- script.js
+++ script.js
@@ -6,8 +6,12 @@ const closeButton = document.querySelector(".close-button");
 const submitBtn = document.querySelector("#modal-submit");
 const deck = document.querySelector("#deck");
 const bookForm = document.querySelector("#bookForm");
-let test = new Book('Lord of the Rings', 'J.R.R. Tolkien', 255, "Finished");
+//const radioButtons = document.querySelectorAll('input');
 
+//radioButtons.addEventListener(onchange, )
+
+let test = new Book('Lord of the Rings', 'J.R.R. Tolkien', 1178, "Finished");
+let test2 = new Book('The Hobbit', 'J.R.R. Tolkien', 310, "Started");
 
 function Book(title, author, pages, read) {
   this.title = title;
@@ -20,10 +24,13 @@ Book.prototype.info = function() {
     return `${this.title} by ${this.author}, ${this.pages} pages. I ${this.read} it.`
 }
 
+Book.prototype.changeStatus = function(status) {
+    this.read = status;
+}
+
 
 function addBookToLibrary(book) {
     myLibrary.push(book);
-  // do stuff here
 }
 
 function addBookThruModal(e) {
@@ -34,11 +41,11 @@ function addBookThruModal(e) {
     const read = document.querySelector('input[name="read"]:checked').value;
     book = new Book(title, author, pages, read);
     addBookToLibrary(book);
+    toggleModal();
     displayBooks();
 }
 
 function displayBooks(){
-    
     while (deck.firstChild) {
         deck.removeChild(deck.lastChild);
       }
@@ -56,16 +63,79 @@ function displayBooks(){
         pages.classList.add('pages');
         pages.textContent = book.pages + " pages";
 
-        const read = document.createElement("div");
-        pages.classList.add('read');
-        read.textContent = book.read;
-    
+        const readOptions = document.createElement("div");
+        readOptions.classList.add('read-options');
+
+        const list = document.createElement("ul");
+        const li1 = document.createElement("li");
+        const rad1 = document.createElement('input');
+        rad1.classList.add('radio');
+        rad1.type = 'radio';
+        rad1.id = 'no';
+        rad1.name = 'read'+book.title;
+        rad1.value = 'Not started';
+      
+        const label1 = document.createElement('label');
+        label1.htmlFor = 'no';
+        label1.textContent = " Not yet started";
+        if (book.read == 'Not started') {
+            rad1.checked = true;
+        }
+
+        const li2 = document.createElement("li");
+        const rad2 = document.createElement('input');
+        rad2.classList.add('radio');
+        rad2.type = 'radio';
+        rad2.id = 'started';
+        rad2.name ='read'+book.title;
+        rad2.value = 'Started';
+        rad2.onchange = book.changeStatus;
+        const label2 = document.createElement('label');
+        label2.htmlFor = 'read';
+        label2.textContent = " Started";
+        if (book.read == 'Started') {
+            rad2.checked = true;
+        }
+
+
+        const li3 = document.createElement("li");
+        const rad3 = document.createElement('input');
+        rad3.classList.add('radio');
+        rad3.type = 'radio';
+        rad3.id = 'finished';
+        rad3.name = 'read'+book.title;
+        rad3.value = 'Finished';
+        rad3.onchange = book.changeStatus();
+        const label3 = document.createElement('label');
+        label3.htmlFor = 'read';
+        label3.textContent = "  Finished";
+        if (book.read == 'Finished') {
+            rad3.checked = true;
+        }
+  
+        li1.append(rad1);
+        li1.append(label1);
+        li2.append(rad2);
+        li2.append(label2);
+        li3.append(rad3);
+        li3.append(label3)
+        list.append(li1);
+        list.append(li2);
+        list.append(li3);
+        readOptions.append(list);
+
+        console.log(list)
+
+
         const card = document.createElement("div");
         card.classList.add('card');
         card.appendChild(title);
         card.appendChild(author);
         card.appendChild(pages);
-        card.appendChild(read);
+        card.appendChild(readOptions);
+       
+
+
         document.querySelector('#deck').appendChild(card);  
     }
 }
@@ -85,7 +155,6 @@ bookForm.addEventListener("submit", addBookThruModal);
 addBookBtn.addEventListener("click", toggleModal);
 closeButton.addEventListener("click", toggleModal);
 
-
-
 addBookToLibrary(test);
+addBookToLibrary(test2);
 displayBooks();
